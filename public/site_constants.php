5de187af30ad41cd41dbf117f64f93bbafdeb380
Hayes <njoguhrk@gmail.com>
2018-04-09 15:50:10 +0300

    Initial commit

---
+++ public/site_constants.php
@@ -0,0 +1,10 @@
+<?php
+           defined('DATEFORMAT_PHP') || define('DATEFORMAT_PHP','d F Y');
+           defined('DATEFORMAT_MYSQL') || define('DATEFORMAT_MYSQL','%d %M %Y');
+           defined('DATEFORMAT_JS') || define('DATEFORMAT_JS','dd MM yy');
+           defined('DATE_DESCRIPTION') || define('DATE_DESCRIPTION','Day, textual month and year');
+           defined('TIME_FORMAT') || define('TIME_FORMAT','H:i:s');   
+           defined('TIMEZONE_OFFSET') || define('TIMEZONE_OFFSET','+03:00');   
+           defined('CURRENCY_FORMAT') || define('CURRENCY_FORMAT','Shillings(Kshs)');
+           defined('PASSWORD_FORMAT') || define('PASSWORD_FORMAT','Alphanumeric and Special Characters');
+        ?>
